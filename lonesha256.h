be660885aaa51497978f15217f2c705b0d6e7d95
BareRose <barerose@users.noreply.github.com>
2018-05-05 18:30:05 +0200

    Cleaned up code, added small test

--- lonesha256.h
+++ lonesha256.h
@@ -1,13 +1,14 @@
 /*
-Lonesha256.h - Portable, endian-proof, single-file, single-function sha256 implementation, originally based on LibTomCrypt
+lonesha256.h - Portable, endian-proof, single-file, single-function sha256 implementation, originally based on LibTomCrypt
 
-To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide.
-This software is distributed without any warranty.
-You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring
+rights to this software to the public domain worldwide. This software is distributed without any warranty.
+You should have received a copy of the CC0 Public Domain Dedication along with this software.
+If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 */
 
 /*
-Lonesha256 supports the following three configurations:
+lonesha256 supports the following three configurations:
 #define LONESHA256_EXTERN
     Default, should be used when using lonesha256 in multiple compilation units within the same project.
 #define LONESHA256_IMPLEMENTATION
@@ -15,11 +16,15 @@ Lonesha256 supports the following three configurations:
 #define LONESHA256_STATIC
     Defines the lonesha256() function as static, useful if lonesha256 is only used in a single compilation unit.
 
-Lonesha256 function:
-(static|extern) int lonesha256 (unsigned char* out, const unsigned char* in, unsigned long inlen)
-    Writes the sha256 hash of the first "inlen" bytes in buffer "in" to buffer "out".
-    Returns 0 for success, may return non-zero in future versions to indicate error.
+lonesha256 function:
+(static|extern) int lonesha256 (unsigned char out[32], const unsigned char* in, size_t len)
+    writes the sha256 hash of the first "len" bytes in buffer "in" to buffer "out"
+    returns 0 on success, may return non-zero in future versions to indicate error
 */
+
+//include only once
+#ifndef LONESHA256_H
+#define LONESHA256_H
 
 //process configuration
 #ifdef LONESHA256_STATIC
@@ -29,26 +34,27 @@ Lonesha256 function:
     #define LSHA256DEF extern
 #endif
 
-//lonesha256 declaration
-LSHA256DEF int lonesha256(unsigned char*, const unsigned char*, unsigned long);
+//includes
+#include <stddef.h> //size_t
 
+//lonesha256 declaration
+LSHA256DEF int lonesha256(unsigned char[32], const unsigned char*, size_t);
+
+//implementation section
 #ifdef LONESHA256_IMPLEMENTATION
+
 //includes
 #include <stdint.h> //uint32_t, uint64_t
 #include <string.h> //memcpy
 
 //macros
-#define Ch(x,y,z) (z ^ (x & (y ^ z)))
-#define Maj(x,y,z) (((x | y) & z) | (x & y))
 #define S(x, n) (((((uint32_t)(x)&0xFFFFFFFFUL)>>(uint32_t)((n)&31))|((uint32_t)(x)<<(uint32_t)((32-((n)&31))&31)))&0xFFFFFFFFUL)
 #define R(x, n) (((x)&0xFFFFFFFFUL)>>(n))
-#define Sigma0(x) (S(x, 2) ^ S(x, 13) ^ S(x, 22))
-#define Sigma1(x) (S(x, 6) ^ S(x, 11) ^ S(x, 25))
 #define Gamma0(x) (S(x, 7) ^ S(x, 18) ^ R(x, 3))
 #define Gamma1(x) (S(x, 17) ^ S(x, 19) ^ R(x, 10))
 #define RND(a,b,c,d,e,f,g,h,i) \
-    t0 = h + Sigma1(e) + Ch(e, f, g) + K[i] + W[i]; \
-    t1 = Sigma0(a) + Maj(a, b, c); \
+    t0 = h + (S(e, 6) ^ S(e, 11) ^ S(e, 25)) + (g ^ (e & (f ^ g))) + K[i] + W[i]; \
+    t1 = (S(a, 2) ^ S(a, 13) ^ S(a, 22)) + (((a | b) & c) | (a & b)); \
     d += t0; \
     h  = t0 + t1;
 #define STORE32H(x, y) \
@@ -73,9 +79,9 @@ LSHA256DEF int lonesha256(unsigned char*, const unsigned char*, unsigned long);
     for (int i = 0; i < 8; i++) sha256_state[i] = sha256_state[i] + S[i];
 
 //lonesha256 function
-LSHA256DEF int lonesha256 (unsigned char* out, const unsigned char* in, unsigned long inlen) {
-    //Writes the sha256 hash of the first "inlen" bytes in buffer "in" to buffer "out".
-    //Returns 0 for success, may return non-zero in future versions to indicate error.
+LSHA256DEF int lonesha256 (unsigned char out[32], const unsigned char* in, size_t len) {
+    //writes the sha256 hash of the first "len" bytes in buffer "in" to buffer "out"
+    //returns 0 on success, may return non-zero in future versions to indicate error
     const uint32_t K[64] = {
         0x428a2f98UL, 0x71374491UL, 0xb5c0fbcfUL, 0xe9b5dba5UL,
         0x3956c25bUL, 0x59f111f1UL, 0x923f82a4UL, 0xab1c5ed5UL,
@@ -98,40 +104,28 @@ LSHA256DEF int lonesha256 (unsigned char* out, const unsigned char* in, unsigned
     uint32_t sha256_state[8] = {
         0x6A09E667UL, 0xBB67AE85UL, 0x3C6EF372UL, 0xA54FF53AUL,
         0x510E527FUL, 0x9B05688CUL, 0x1F83D9ABUL, 0x5BE0CD19UL
-    }, sha256_curlen = 0, S[8], W[64], t0, t1, t;
+    }, S[8], W[64], t0, t1, t;
     unsigned char sha256_buf[64];
-    unsigned long n;
-    //process input
-    while (inlen > 0) {
-        if (sha256_curlen == 0 && inlen >= 64) {
-           SHA256_COMPRESS(in);
-           sha256_length += 64 * 8;
-           in += 64;
-           inlen -= 64;
-        } else {
-           n = (inlen < 64 - sha256_curlen) ? inlen : (64 - sha256_curlen);
-           memcpy(sha256_buf + sha256_curlen, in, n);
-           sha256_curlen += n;
-           in += n;
-           inlen -= n;
-           if (sha256_curlen == 64) {
-              SHA256_COMPRESS(sha256_buf);
-              sha256_length += 8*64;
-              sha256_curlen = 0;
-           }
-       }
+    //process input in 64 byte chunks
+    while (len >= 64) {
+       SHA256_COMPRESS(in);
+       sha256_length += 64 * 8;
+       in += 64;
+       len -= 64;
     }
-    //finish up
-    sha256_length += sha256_curlen * 8;
-    sha256_buf[sha256_curlen++] = 0x80;
+    //copy remaining bytes into sha256_buf
+    memcpy(sha256_buf, in, len);
+    //finish up (len now number of bytes in sha256_buf)
+    sha256_length += len * 8;
+    sha256_buf[len++] = 0x80;
     //pad then compress if length is above 56 bytes
-    if (sha256_curlen > 56) {
-        while (sha256_curlen < 64) sha256_buf[sha256_curlen++] = 0;
+    if (len > 56) {
+        while (len < 64) sha256_buf[len++] = 0;
         SHA256_COMPRESS(sha256_buf);
-        sha256_curlen = 0;
+        len = 0;
     }
     //pad up to 56 bytes
-    while (sha256_curlen < 56) sha256_buf[sha256_curlen++] = 0;
+    while (len < 56) sha256_buf[len++] = 0;
     //store length and compress
     STORE64H(sha256_length, sha256_buf + 56);
     SHA256_COMPRESS(sha256_buf);
@@ -142,4 +136,6 @@ LSHA256DEF int lonesha256 (unsigned char* out, const unsigned char* in, unsigned
     //return
     return 0;
 }
-#endif
+
+#endif //LONESHA256_IMPLEMENTATION
+#endif //LONESHA256_H
